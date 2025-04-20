4ae387915a1545ef931dd92db0be3c685240dea9
Myles Jefferson <99mylesjefferson@gmail.com>
2025-04-20 17:01:13 -0600

    feat: Implement health check and chirp validation endpoints; add client.rest for API testing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"sync/atomic"
+	"encoding/json"
 )
 
 type apiConfig struct {
@@ -19,9 +20,16 @@ func (cfg *apiConfig) handlerMetrics(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits: " + fmt.Sprint(cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+	<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>
+	`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,6 +37,61 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
+}
+
+func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type response struct {
+		Valid bool `json:"valid"`
+		Error string `json:"error,omitempty"`
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		resp := response{
+			Valid: false,
+			Error: "Bad Request",
+		}
+		jsonResp, _ := json.Marshal(resp)
+		w.Write(jsonResp)
+		return
+	}
+
+	//  Check to see if the body is greater than 140 characters
+	if len(params.Body) > 140 {
+		w.WriteHeader(http.StatusBadRequest)
+		resp := response{
+			Valid: false,
+			Error: "Chirp is too long",
+		}
+		jsonResp, _ := json.Marshal(resp)
+		w.Write(jsonResp)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	resp := response{
+		Valid: true,
+	}
+	jsonResp, _ := json.Marshal(resp)
+	w.Write(jsonResp)
 }
 
 func main() {
@@ -40,19 +103,18 @@ func main() {
 		http.ServeFile(w, r, "index.html")
 	})))
 
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	mux.HandleFunc("GET /api/healthz", handleHealthCheck)
 
 	// Serve static files from the assets directory
 	fs := http.FileServer(http.Dir("assets"))
 	mux.Handle("/app/assets/", apiCfg.handlerMetrics(http.StripPrefix("/app/assets/", fs)))
 
 	// Register metrics and reset endpoints
-	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("POST /reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+
+
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
